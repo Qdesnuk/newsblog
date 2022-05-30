aa08c956093c153734eeebb5e5e6da3536a14c65
Andrey <AndreyHlopkov@gmaile.com>
2022-05-30 20:11:50 +0300

    init

---
+++ tooster/news/views.py
@@ -0,0 +1,127 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import News, Category
+from .forms import NewsForm, UserRegisterForm, UserLoginForm
+from .utils import MyMixin
+from django.core.paginator import Paginator
+
+from django.contrib import messages
+from django.contrib.auth import login, logout
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Вы успешно зарегистрировались')
+            return redirect('home')
+        else:
+            messages.error(request, 'Ошибка регистрации')
+    else:
+        form = UserRegisterForm()
+    return render(request, template_name='news/register.html', context={'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserLoginForm()
+    return render(request, template_name='news/login.html', context={"form": form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+
+class HomeNews(MyMixin, ListView):
+    model = News
+    paginate_by = 3
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.get_upper('Главная страница')
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True).select_related('category')
+
+
+class NewsByCategory(MyMixin, ListView):
+    model = News
+    allow_empty = False
+    paginate_by = 2
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True).select_related('category')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.get_upper(Category.objects.get(pk=self.kwargs['category_id']))
+        return context
+
+
+class ViewNews(DetailView):
+    model = News
+    # pk_url_kwarg = 'news_id'
+    # template_name = 'news/news_detail.html'
+    context_object_name = 'news_item'
+
+
+class CreateNews(LoginRequiredMixin, CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    login_url = '/admin/'
+
+
+# def index(request):
+#     news = News.objects.all()
+#     index_context = {
+#         'news': news,
+#         'title': 'Список новостей',
+#     }
+#     return render(request, template_name='news/index.html', context=index_context)
+
+
+# def get_category(request, category_id):
+#     news = News.objects.filter(category_id=category_id)
+#     category = Category.objects.get(pk=category_id)
+#     category_context = {
+#         'news': news,
+#         'category': category,
+#     }
+#     return render(request, template_name='news/category.html', context=category_context)
+
+
+# def view_news(request, news_id):
+#     # news_item = News.objects.get(pk=news_id)
+#     news_item = get_object_or_404(News, pk=news_id)
+#     view_news_context = {
+#         'news_item': news_item,
+#     }
+#     return render(request, template_name='news/view_news.html', context=view_news_context)
+
+
+# def add_news(request):
+#     if request.method == 'POST':
+#         form = NewsForm(request.POST)
+#         if  form.is_valid():
+#             # print(form.cleaned_data)
+#             # news = News.objects.create(**form.cleaned_data)
+#             news = form.save()
+#             return redirect(news)
+#     else:
+#         form = NewsForm()
+#
+#     form_context = {
+#         'form': form
+#     }
+#     return render(request, template_name='news/add_news.html', context=form_context)
